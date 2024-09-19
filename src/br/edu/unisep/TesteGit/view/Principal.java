600f16675dd66559e7e03981098b8f53f1a5ece8
Apollo-Hb <apollohb0@gmail.com>
2024-09-19 19:49:08 -0300

    add Class joia

---
+++ src/br/edu/unisep/TesteGit/view/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.unisep.TesteGit.view;
+
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(        null,
+                "Mensagem teste!"
+        );
+    }
+}
+
